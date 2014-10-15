484a4282ac16e430660357c605e27292e83cb323
Vitaly Bondaruk <vitbondaruk@gmail.com>
2014-10-15 18:11:46 +0300

    measures, window_size

--- phrases.py
+++ phrases.py
@@ -1,6 +1,8 @@
 from __future__ import division
 import nltk, os, sys
 from HTMLParser import HTMLParser
+import itertools as _itertools
+
 
 def strip_tags(html):
     result = []
@@ -10,7 +12,25 @@ def strip_tags(html):
     parser.close()
     return ''.join(result)
 
-if len(sys.argv)>1:
+
+def find_phrases(documents, finders):
+    ignored_words = nltk.corpus.stopwords.words('english')
+
+    for finder in finders:
+        f = finder['finder']
+        f = f.from_documents(documents)
+
+        f.apply_word_filter(lambda w: len(w) < 3 or w.lower() in ignored_words)
+        f.apply_freq_filter(finder['freq_min'])
+
+        ngrams = f.score_ngrams(getattr(finder['measures'], finder['measure']))[:20]
+
+        print("--- %s ---" % finder['measure'])
+        print("\n".join([' '.join(x[0]) + (' [%02f]' % x[1]) for x in ngrams]))
+    print("\n")
+
+
+if len(sys.argv) > 1:
     dir = sys.argv[1]
     if not os.path.isdir(dir):
         print("Dir not exists")
@@ -27,23 +47,14 @@ for f in os.listdir(dir):
         tokens = nltk.word_tokenize(str)
         documents.append(tokens)
 
-finders = [[nltk.BigramCollocationFinder, nltk.metrics.association.BigramAssocMeasures().likelihood_ratio, 10]]
-finders += [[nltk.TrigramCollocationFinder, nltk.metrics.association.TrigramAssocMeasures().likelihood_ratio, 5]]
-finders += [[nltk.QuadgramCollocationFinder, nltk.metrics.association.QuadgramAssocMeasures().likelihood_ratio, 3]]
+#finders = [{'finder': nltk.BigramCollocationFinder, 'measures': nltk.metrics.association.BigramAssocMeasures(), 'measure': 'likelihood_ratio', 'freq_min': 10}]
+#finders += [{'finder': nltk.TrigramCollocationFinder, 'measures': nltk.metrics.association.TrigramAssocMeasures(), 'measure': 'likelihood_ratio', 'freq_min': 5}]
+#finders += [{'finder': nltk.QuadgramCollocationFinder, 'measures': nltk.metrics.association.QuadgramAssocMeasures(), 'measure': 'likelihood_ratio', 'freq_min': 3}]
 
-ignored_words = nltk.corpus.stopwords.words('english')
+ngram = 'Bigram'
 
-for finder in finders:
-    freq_filter = finder[2]
-    ngram_measure = finder[1]
-    finder = finder[0]
-    finder = finder.from_documents(documents)
-    finder.apply_word_filter(lambda w: len(w) < 3 or w.lower() in ignored_words)
-    finder.apply_freq_filter(freq_filter)
+measures = ["raw_freq"]
+finders = [{'finder': getattr(nltk, ngram + 'CollocationFinder'), 'measures': getattr(nltk.metrics.association, ngram + 'AssocMeasures')(), 'measure': x, 'freq_min': 10} for x in measures]
 
-    ngrams = finder.nbest(ngram_measure, 20)
+find_phrases(documents, finders)
 
-    print("---")
-    print("\n".join([' '.join(x) for x in ngrams]))
-
-print("\n")
