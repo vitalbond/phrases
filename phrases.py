1edd02a4b84623c4780a23fb8e1ee55a74b6e62c
Vitaly Bondaruk <vitbondaruk@gmail.com>
2014-10-16 17:29:36 +0300

    stopwords

--- phrases.py
+++ phrases.py
@@ -1,5 +1,5 @@
 from __future__ import division
-import nltk, os, sys
+import nltk, os, sys, re
 from HTMLParser import HTMLParser
 import itertools as _itertools
 
@@ -13,22 +13,31 @@ def strip_tags(html):
     return ''.join(result)
 
 
-def find_phrases(documents, finders):
-    ignored_words = nltk.corpus.stopwords.words('english')
+_re_non_punct = re.compile(r'[^\W\d]', re.UNICODE)
+def is_punct(w):
+    return not _re_non_punct.search(w)
+
+
+def find_phrases(documents, finders, words_filter = 'punct', window_size = 2, freq_min = 0):
+    stopwords = nltk.corpus.stopwords.words('english')
 
     for finder in finders:
         f = finder['finder']
-        f = f.from_documents(documents)
+        f = f.from_words(_itertools.chain(*documents), window_size)
 
-        f.apply_word_filter(lambda w: len(w) < 3 or w.lower() in ignored_words)
-        f.apply_freq_filter(finder['freq_min'])
+        if words_filter=='punct':
+            f.apply_word_filter(lambda w: is_punct(w))
+        elif words_filter=='stopwords':
+            f.apply_word_filter(lambda w: len(w) < 3 or w.lower() in stopwords)
+
+        if freq_min:
+            f.apply_freq_filter(freq_min)
 
         ngrams = f.score_ngrams(getattr(finder['measures'], finder['measure']))[:20]
 
         print("--- %s ---" % finder['measure'])
         print("\n".join([' '.join(x[0]) + (' [%02f]' % x[1]) for x in ngrams]))
     print("\n")
-
 
 if len(sys.argv) > 1:
     dir = sys.argv[1]
@@ -39,11 +48,14 @@ else:
     print("Dir is not set")
     exit()
 
+print "\n"
+
 documents = []
 for f in os.listdir(dir):
     file = os.path.join(dir, f)
     if os.path.isfile(file):
         str = strip_tags(open(file, 'r').read()).decode('utf-8')
+        print str
         tokens = nltk.word_tokenize(str)
         documents.append(tokens)
 
@@ -54,7 +66,15 @@ for f in os.listdir(dir):
 ngram = 'Bigram'
 
 measures = ["raw_freq"]
-finders = [{'finder': getattr(nltk, ngram + 'CollocationFinder'), 'measures': getattr(nltk.metrics.association, ngram + 'AssocMeasures')(), 'measure': x, 'freq_min': 10} for x in measures]
 
-find_phrases(documents, finders)
+#student_t
+#chi_sq
+#mi_like
+#pmi
+#likelihood_ratio
+#poisson_stirling
+#jaccard
+
+finders = [{'finder': getattr(nltk, ngram + 'CollocationFinder'), 'measures': getattr(nltk.metrics.association, ngram + 'AssocMeasures')(), 'measure': x} for x in measures]
+find_phrases(documents, finders, freq_min=0, words_filter='punct')
 
